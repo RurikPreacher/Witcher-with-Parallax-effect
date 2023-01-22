0d67c37f07edd5b5632472c0b68d1b167f090470
RurikPreacher <RurikPreacher@gmail.com>
2023-01-23 01:19:13 +0200

    Witcher with a horizontal Parallax effect

---
+++ js/app.js
@@ -0,0 +1,11 @@
+new Swiper('.slider', {
+    // direction: 'vertical',
+    // freeMode: true,
+    speed: 2400,
+    mousewheel: {
+        enable: true,
+        sensitivity: 2.4,
+    },
+    spaceBetween: 18,
+    parallax: true,
+})
